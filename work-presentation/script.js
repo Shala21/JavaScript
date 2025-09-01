b3cfd4bb45172788f3381eb21f372f6f0bc01056
Shala <119969733+Shala21@users.noreply.github.com>
2025-09-01 18:16:20 +0200

    feat:  methods js presentation

---
+++ work-presentation/script.js
@@ -0,0 +1,15 @@
+//**
+// .map & .filter()  ->
+
+// map()
+// Crea un nuovo array trasformando ogni elemento dell’array originale secondo una funzione.
+
+// filter()
+// Restituisce un nuovo array con solo gli elementi che soddisfano una condizione.
+
+ //*
+
+ const number = [1, 2, 3, 4, 5, 6, 7];
+
+ const num = number.map(n => n * 2);
+ console.log(num);
